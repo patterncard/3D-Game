3e1c4293c165adffdfc7a949daab5a90dfd0ad9e
Patrycja Cader <patcad@o2.pl>
2020-07-19 21:26:36 +0200

    initial commit

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour {
+    private int requiredCoins;
+    private Game game;
+	// Use this for initialization
+	void Start () {
+        game = FindObjectOfType<Game>();
+        requiredCoins = GameObject.FindGameObjectsWithTag("Coin").Length;
+	}
+	
+    public void CheckForCompletion(int coinCount)
+    {
+        if (coinCount >= requiredCoins)
+        {
+            game.LoadNextLevel();
+        }
+        else
+        {
+            Debug.LogFormat("You need more coins! You have {0} out of {1}", coinCount, requiredCoins);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody playerBody;
+    [SerializeField]
+    private TMPro.TextMeshProUGUI coinText;
+    private GameObject sword;
+    private Game game;
+    private bool jump;
+    [SerializeField]
+    private int coins;
+
+    private Vector3 inputVector;
+	// Use this for initialization
+	void Start ()
+    {
+        sword = transform.GetChild(0).gameObject;
+        game = FindObjectOfType<Game>();
+        playerBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    { 
+        inputVector = new Vector3(Input.GetAxis("Horizontal") * 10f, playerBody.velocity.y, Input.GetAxis("Vertical") * 10f);
+        transform.LookAt(transform.position + new Vector3(inputVector.x, 0, inputVector.z));
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+        if (Input.GetButtonDown("Attack"))
+        {
+            PerformAttack();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        playerBody.velocity = inputVector;
+        if (jump && IsGrounded())
+        {
+            playerBody.AddForce(Vector3.up * 20f, ForceMode.Impulse);
+            jump = false;
+        }
+    }
+    
+    private void PerformAttack()
+    {
+        if (!sword.activeSelf)
+        {
+            sword.SetActive(true);
+        }
+    }
+
+    bool IsGrounded()
+    {
+        float distance = GetComponent<Collider>().bounds.extents.y + 0.01f;
+        Ray ray = new Ray(transform.position, Vector3.down);
+        return Physics.Raycast(ray, distance);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            game.ReloadCurrentLevel();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        switch (other.tag)
+        {
+            case "Coin":
+                coins++;
+                coinText.text = string.Format("Coins\n{0}", coins);
+                Destroy(other.gameObject);
+                break;
+            case "Goal":
+                other.GetComponent<Goal>().CheckForCompletion(coins);
+                break;
+            default:
+                break;
+        }
+    }
+}
