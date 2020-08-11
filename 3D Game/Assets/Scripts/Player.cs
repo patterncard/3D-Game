c0ac607135573fb77e638235b90ad43f137c7945
Patrycja Cader <patcad@o2.pl>
2020-08-11 22:34:07 +0200

    fixing bugs

--- 3D Game/Assets/Scripts/Player.cs
+++ 3D Game/Assets/Scripts/Player.cs
@@ -5,17 +5,18 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField]
-    private Rigitbody playerBody;
+    private Rigidbody playerBody;
+    private Vector3 inputVector;
     // Start is called before the first frame update
     void Start()
     {
-        playerBody = GetComponent<Rigitbody>();
+        playerBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        inputVector = new Vector3(Input.GetAxisRaw("Horizontal", playerBody.velocity.y, Input.GetAxisRaw("Vertical")));
+        inputVector = new Vector3(Input.GetAxisRaw("Horizontal"), playerBody.velocity.y, Input.GetAxisRaw("Vertical"));
         playerBody.velocity = inputVector;
     }
 }
