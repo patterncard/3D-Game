50f6f7be8bdc9836527bcba53940cd7ee2e49eb2
Patrycja Cader <patcad@o2.pl>
2020-08-22 21:58:04 +0200

    jumping

--- 3D Game/Assets/Scripts/Player.cs
+++ 3D Game/Assets/Scripts/Player.cs
@@ -6,17 +6,39 @@ public class Player : MonoBehaviour
 {
     [SerializeField]
     private Rigidbody playerBody;
+    private bool jump;
+
     private Vector3 inputVector;
     // Start is called before the first frame update
     void Start()
     {
-        playerBody = GetComponent<Rigidbody>();
+        // playerBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        inputVector = new Vector3(Input.GetAxisRaw("Horizontal"), playerBody.velocity.y, Input.GetAxisRaw("Vertical"));
+        inputVector = new Vector3(Input.GetAxis("Horizontal") * 10f, playerBody.velocity.y, Input.GetAxis("Vertical") * 10f);
+        transform.LookAt(transform.position + new Vector3(inputVector.x, 0, inputVector.z));
+        if(Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
         playerBody.velocity = inputVector;
+        if(jump)
+        {
+            playerBody.AddForce(Vector3.up * 20f, ForceMode.Impulse);
+            jump = false;
+        }
+    }
+
+    bool IsGrounded()
+    {
+        float distance = GetComponent<Collider>().bounds.extents.y + 0.01f;
+        return false;
     }
 }
