630df1d05ef942a1aca0afeaf30ca984bdd8ba28
Patrycja Cader <patcad@o2.pl>
2020-08-28 15:19:12 +0200

    game manager

---
+++ 3D Game/Assets/Scripts/Game.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Game : MonoBehaviour
+{
+    [SerializeField]
+    private int level;
+    [SerializeField]
+    private bool lastLevel;
+    private int nextLevel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextLevel = level + 1;
+    }
+
+    public void LoadLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void LoadNextLevel()
+    {
+        if(!lastLevel)
+        {
+            string sceneName = "Level-" + nextLevel;
+            SceneManager.LoadScene(sceneName);
+        }
+        else
+        {
+            // go to main menu
+            SceneManager.LoadScene("Level-1");
+        }
+
+    }
+
+    public void ReloadCurrentLevel()
+    {
+        SceneManager.LoadScene("Leevel-" + level);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
